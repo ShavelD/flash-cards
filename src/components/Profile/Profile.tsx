9c5fe368732925dcc058733cadb3dc16c64f5891
germashukanna <germashuk@gmail.com>
2022-12-14 13:33:09 +0300

    feat: create Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import s from './Profile.module.css'
+import {SuperInputText} from "../../common/SuperInput/SuperInput";
 
 
 export const Profile = () => {
 
+    const [name, setName] = useState<string>('')
+
+
+
     return (
-        <>
-            Profile
-        </>
+        <div className={s.profile}>
+            <div><a href="">Back to Packs List</a></div>
+            <h2>Personal Information</h2>
+            <div><img src="" alt=""/></div>
+            <SuperInputText/>
+            <button>Log out</button>
+        </div>
     )
 }
